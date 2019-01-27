02e3c994efcbab30d4fb27932507839db05b9b77
Eban Bisong <ebisong18@gmail.com>
2019-01-27 18:42:57 -0500

    Added mongoose support for connection with MongoDB database.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+const Title = require('./lib/models/title');
+const {
+  DB_URL
+} = process.env;
 
+const start = async () => {
+  mongoose.connect(DB_URL, { useNewUrlParser: true });
+  const titles = await Title.find({});
+  console.log(titles);
+};
+
+start();
